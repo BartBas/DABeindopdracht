48e3381435b3ae131bf8b0f75bf180e824e8a694
Flits <kasperjnssen@gmail.com>
2023-10-11 21:12:23 +0200

    Update mainwindow.cpp

--- GUI/mainwindow.cpp
+++ GUI/mainwindow.cpp
@@ -31,7 +31,6 @@ MainWindow::MainWindow(QWidget *parent)
     //connect lists and buttons to functions
     connect(ui->merk, SIGNAL(itemClicked(QListWidgetItem*)),this, SLOT(onBrandClicked(QListWidgetItem*)));
     connect(ui->model, SIGNAL(itemClicked(QListWidgetItem*)),this, SLOT(onModelClicked(QListWidgetItem*)));
-    connect(ui->model, SIGNAL(itemClicked(QListWidgetItem*)),this, SLOT(listModelDetails(QListWidgetItem*)));
 }
 
 void MainWindow::listBrand()
@@ -75,6 +74,7 @@ void MainWindow::listBrandDetails(std::string brandChosen)
         ui->merkInfo->addItem(failed);
         return;
     }
+    ui->merkInfo->clear();
     while(query.next())
     {
         QString stad = query.value(0).toString(); //get stad from the query
@@ -92,11 +92,12 @@ void MainWindow::listModel(std::string brand)
 {
     db.open();
     QSqlQuery query;
-    query.prepare("SELECT tblModel.strType, tblUitvoering.strUitvoering FROM tblAuto "
+    query.prepare("SELECT tbltype.strType, tblUitvoering.strUitvoering FROM tblAuto "
                   "JOIN tblModel ON tblAuto.TypeID = tblModel.ID "
-                  "JOIN tblUitvoering ON tblModel.UitvoeringID = tblUitvoering.ID "
+                  "JOIN tblType ON tblModel.TypeID = tblType.ID "
+                  "LEFT JOIN tblUitvoering ON tblModel.UitvoeringID = tblUitvoering.ID "
                   "JOIN tblMerk ON tblAuto.MerkID = tblMerk.ID "
-                  "WHERE tblMerk.Merk = :brand ");
+                  "WHERE tblMerk.strMerk = :brand ");
     query.bindValue(":brand", QString::fromStdString(brand));
     if(!query.exec())
     {
@@ -105,14 +106,22 @@ void MainWindow::listModel(std::string brand)
         ui->model->addItem(failed);
         return;
     }
+    ui->model->clear();
     while(query.next())
     {
         QString type = query.value(0).toString(); //get type from the query
         QString uitvoering = query.value(1).toString(); //get uitvoering from the query
 
-        QString model = type + " " + uitvoering; //combine type and uitvoering for the model
+
+        QString model = type + " ";
+
+        if(uitvoering != "")
+        {
+            model = model + uitvoering; //combine type and uitvoering for the model
+        }
 
         QListWidgetItem *item = new QListWidgetItem(model);
+
         ui->model->addItem(item);
     }
     db.close();
@@ -121,16 +130,27 @@ void MainWindow::listModelDetails(std::string model)
 {
     db.open();
     QSqlQuery query;
-    query.prepare("SELECT tblVermogen.PK, tblVermogen.KW FROM tblModel"
-                  "JOIN tblVermogen ON tblModel.vermogenID = tblVermogen.ID"
-                  "WHERE tblModel = :modelChosen");
+    query.prepare("SELECT tblVermogen.Vermogen FROM tblModel "
+                  "JOIN tblVermogen ON tblModel.vermogenID = tblVermogen.ID "
+                  "JOIN tblType ON tblModel.TypeID = tblType.ID "
+                  "LEFT JOIN tblUitvoering ON tblModel.UitvoeringID = tblUitvoering.ID "
+                  "WHERE CONCAT(tblType.strType, IF(tblUitvoering.strUitvoering IS NOT NULL, CONCAT(\" \", tblUitvoering.strUitvoering), \"\")) =  :modelChosen ");
     query.bindValue(":modelChosen", QString::fromStdString(model));
+    if(!query.exec())
+    {
+        qDebug() << "Query failed:" << query.lastError().text();
+        QListWidgetItem *failed = new QListWidgetItem("Query failed");
+        ui->model->addItem(failed);
+        return;
+    }
+    ui->modelDetails->clear();
     while(query.next())
     {
-        QString PK = query.value(0).toString(); //get PK from query
-        QString KW = query.value(1).toString(); //get KW from query
+        QString KW = query.value(0).toString(); //get PK from query
+        float KWFloat = KW.toFloat();
+        float KWCalculated = round(KWFloat*(1.3625));
 
-        QString vermogen = "PK:" + PK + "& KW:" + KW; //combine PK and KW with context for reader
+        QString vermogen = "PK: " + QString::number(KWCalculated) + " & KW: " + KW; //combine PK and KW with context for reader
 
         QListWidgetItem *item = new QListWidgetItem(vermogen);
         ui->modelDetails->addItem(item);
@@ -143,23 +163,17 @@ void MainWindow::brandLogo(std::string brandChosen)
     db.open();
     QSqlQuery query;
     //query.prepare("SELECT Plaatje FROM tblModel WHERE ID = 1"); //code used to test
-    query.prepare("SELECT plaatje FROM tblMerk "
+    query.prepare("SELECT Plaatje FROM tblMerk "
                   "WHERE strMerk = :chosen");
     query.bindValue(":chosen", QString::fromStdString(brandChosen));
     if(query.exec() && query.next())
     {
         QByteArray blobData = query.value(0).toByteArray();
 
-        QImageReader imageReader;
-        imageReader.setDevice(new QBuffer(&blobData));
-        QImage image = imageReader.read();
-
-        if(image.isNull())
-        {
-            qDebug() << "Failed to load image from blob";
-            return;
-        }
-        ui->logo->setPixmap(QPixmap::fromImage(image));
+        QImage image;
+        image.loadFromData(QByteArray::fromBase64(blobData));
+        QImage image2 = image.scaled(200, 200, Qt::KeepAspectRatio);
+        ui->logo->setPixmap(QPixmap::fromImage(image2));
         ui->logo->setAlignment(Qt::AlignCenter);
     }
     else
@@ -192,5 +206,3 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
-
-
